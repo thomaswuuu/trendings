29b5f5d70274eac1aa411ce77cf6139df701c474
numb1086 <numb1086@users.noreply.github.com>
2023-10-06 15:40:38 +0800

    1. trends.css - Set results class max-width value 2. explore.css - Set searches class max-width and font-size value 3. explore.js - Add RelatedTopics function 4. explore.ejs - Add Related Topic object and views 5. index.ejs - Add results class

--- routes/explore.js
+++ routes/explore.js
@@ -3,10 +3,46 @@ const router = express.Router();
 const regions = require("../models/regions");
 const googleTrends = require("google-trends-api");
 
+function getRelatedTopics(
+  res,
+  keyword,
+  relatedKeywords,
+  geoLocation = "TW",
+  start
+) {
+  googleTrends
+    .relatedTopics({
+      keyword: keyword,
+      geo: geoLocation,
+      startTime: new Date(start),
+      hl: "zh-TW",
+    })
+    .then((results) => {
+      let rankedList = JSON.parse(results).default.rankedList;
+      let rankedTopic = rankedList[0].rankedKeyword;
+      let recentTopic = rankedList[1].rankedKeyword;
+      res.render("explore", {
+        regions: regions,
+        query: keyword,
+        geoLocation: geoLocation,
+        rankedKeyword: relatedKeywords[0],
+        recentKeyword: relatedKeywords[1],
+        rankedTopic: rankedTopic,
+        recentTopic: recentTopic,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
 function getRelatedQueries(res, keyword, geoLocation = "TW") {
-  // TODO: Get the date of 12 month ago from now
-  let start = "2023-01-01";
-  //
+  // Get the date of 12 month ago from now
+  let start = new Date();
+  start = new Date(start.setFullYear(start.getFullYear() - 1));
+  // Set yyyy-mm-dd format;
+  start = start.toISOString().slice(0, 10);
+  console.log(start);
   googleTrends
     .relatedQueries({
       keyword: keyword,
@@ -14,16 +50,12 @@ function getRelatedQueries(res, keyword, geoLocation = "TW") {
       startTime: new Date(start),
     })
     .then((results) => {
+      console.log(results);
       let rankedList = JSON.parse(results).default.rankedList;
       let rankedKeyword = rankedList[0].rankedKeyword;
       let recentKeyword = rankedList[1].rankedKeyword;
-      res.render("explore", {
-        regions: regions,
-        query: keyword,
-        geoLocation: geoLocation,
-        rankedKeyword: rankedKeyword,
-        recentKeyword: recentKeyword,
-      });
+      let relatedKeywords = [rankedKeyword, recentKeyword];
+      getRelatedTopics(res, keyword, relatedKeywords, geoLocation, start);
     })
     .catch((err) => {
       console.log(err);
